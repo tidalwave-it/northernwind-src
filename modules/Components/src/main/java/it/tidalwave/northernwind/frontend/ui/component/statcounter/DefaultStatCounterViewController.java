c234b369ff8eddd1ff077f5e3f24a8aee71d8696
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2011-07-24 10:16:53 +0200

    Add StatCounter component.

---
+++ modules/Components/src/main/java/it/tidalwave/northernwind/frontend/ui/component/statcounter/DefaultStatCounterViewController.java
@@ -0,0 +1,55 @@
+/***********************************************************************************************************************
+ *
+ * NorthernWind - lightweight CMS
+ * Copyright (C) 2011-2011 by Tidalwave s.a.s. (http://www.tidalwave.it)
+ *
+ ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************
+ *
+ * WWW: http://northernwind.java.net
+ * SCM: http://java.net/hg/northernwind~src
+ *
+ **********************************************************************************************************************/
+package it.tidalwave.northernwind.frontend.ui.component.statcounter;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import it.tidalwave.northernwind.frontend.model.SiteNode;
+import it.tidalwave.northernwind.frontend.ui.component.DefaultStaticHtmlFragmentViewController;
+
+/***********************************************************************************************************************
+ *
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+public class DefaultStatCounterViewController extends DefaultStaticHtmlFragmentViewController implements StatCounterViewController
+  {  
+    /*******************************************************************************************************************
+     *
+     * Creates an instance for populating the given {@link StatCounterView} with the given {@link SiteNode}.
+     * 
+     * @param  view              the related view
+     * @param  viewInstanceName  the name of the view instance
+     * @param  siteNode          the related {@link SiteNode}
+     *
+     ******************************************************************************************************************/
+    public DefaultStatCounterViewController (final @Nonnull StatCounterView view, 
+                                             final @Nonnull String viewInstanceName,
+                                             final @Nonnull SiteNode siteNode) 
+      throws IOException 
+      {
+        super(view, viewInstanceName, siteNode);
+        populate("StatCounterHtmlFragment.html"); // FIXME: interpolate with properties
+      }
+  }
