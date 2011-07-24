d637f173327ffb1028e53e4dd66748e955423518
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2011-07-24 09:53:41 +0200

    Added AddThis component.

---
+++ modules/VaadinComponents/src/main/java/it/tidalwave/northernwind/frontend/ui/component/vaadin/VaadinStaticHtmlFragmentView.java
@@ -0,0 +1,55 @@
+/***********************************************************************************************************************
+ *
+ * NorthernWind - lightweight CMS
+ * Copyright (C) 2011-2011 by Tidalwave s.a.s. (http://www.tidalwave.it)
+ *
+ ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************
+ *
+ * WWW: http://northernwind.java.net
+ * SCM: http://java.net/hg/northernwind~src
+ *
+ **********************************************************************************************************************/
+package it.tidalwave.northernwind.frontend.ui.component.vaadin;
+
+import javax.annotation.Nonnull;
+import it.tidalwave.northernwind.frontend.ui.component.StaticHtmlFragmentView;
+import com.vaadin.ui.Label;
+
+/***********************************************************************************************************************
+ *
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+public class VaadinStaticHtmlFragmentView extends Label implements StaticHtmlFragmentView
+  {
+    /*******************************************************************************************************************
+     *
+     * Creates an instance with the given name.
+     * 
+     * @param  name  the component name
+     *
+     ******************************************************************************************************************/
+    public VaadinStaticHtmlFragmentView (final @Nonnull String name) 
+      {
+        setStyleName("component-" + name);
+        setContentMode(Label.CONTENT_RAW);
+      }
+    
+    @Override
+    public void setHtmlFragment (final @Nonnull String html) 
+      {
+        setValue(html);
+      }    
+  }
