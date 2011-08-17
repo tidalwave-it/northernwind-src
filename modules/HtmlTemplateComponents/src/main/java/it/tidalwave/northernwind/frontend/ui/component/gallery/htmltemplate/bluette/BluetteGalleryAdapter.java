c5fb449b2e55d4c2554f027740097e456da2e0e5
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2011-08-17 21:17:57 +0200

    Now reading all the Bluette configuration from the properties.

--- modules/HtmlTemplateComponents/src/main/java/it/tidalwave/northernwind/frontend/ui/component/gallery/htmltemplate/bluette/BluetteGalleryAdapter.java
+++ modules/HtmlTemplateComponents/src/main/java/it/tidalwave/northernwind/frontend/ui/component/gallery/htmltemplate/bluette/BluetteGalleryAdapter.java
@@ -22,20 +22,24 @@
  **********************************************************************************************************************/
 package it.tidalwave.northernwind.frontend.ui.component.gallery.htmltemplate.bluette;
 
-import it.tidalwave.northernwind.core.model.Site;
-import it.tidalwave.northernwind.core.model.SiteNode;
 import javax.annotation.Nonnull;
+import javax.inject.Inject;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import it.tidalwave.util.Id;
+import it.tidalwave.util.NotFoundException;
+import it.tidalwave.util.Key;
+import it.tidalwave.northernwind.core.model.ResourceProperties;
+import it.tidalwave.northernwind.core.model.Site;
+import it.tidalwave.northernwind.core.model.SiteNode;
 import it.tidalwave.northernwind.frontend.ui.component.gallery.htmltemplate.spi.GalleryAdapterContext;
 import it.tidalwave.northernwind.frontend.ui.component.gallery.htmltemplate.spi.GalleryAdapterSupport;
-import javax.inject.Inject;
 import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Configurable;
 
 /***********************************************************************************************************************
  *
@@ -75,21 +79,33 @@ public class BluetteGalleryAdapter extends GalleryAdapterSupport
       {
         final SiteNode siteNode = context.getSiteNode();
         final String link = site.createLink(siteNode.getRelativeUri() + "/images.xml");
-        return "<script type=\"text/javascript\">\n"
-           + "var catalogUrl = \"" + link + "\";\n"
-           + "var photoPrefix = \"http://stoppingdown.net/media/stillimages/\";\n"
-           + "var home = \"/nw/blog/\";\n"
-           + "var titlePrefix = \"Stopping Down: \";\n"
-           + "var slideshowSpeed = 8000;\n"
-           + "var sizes = [1920, 1280, 800, 400, 200];\n"
-           + "var thumbnailsPerRow = 10;\n"
-           + "var availWidthPercentage = 1.0;\n"
-           + "var availHeightPercentage = 0.85;\n"
-           + "var borderScale = 6.0 / 1920.0;\n"
-           + "var captionFontSizeScale = 25.0 / 1280.0;\n"
-           + "var headerFontSizeScale = 25.0 / 1280.0;\n"
-           + "var titleVisible = true;\n"
-           + "var logging = true;\n"
-           + "</script>\n";
+
+        final StringBuilder builder = new StringBuilder();
+        builder.append("<script type=\"text/javascript\">\n");
+        builder.append(String.format("var catalogUrl = \"%s\";\n", link));
+        
+        try 
+          {
+            final ResourceProperties bluetteConfiguration = siteNode.getPropertyGroup(new Id("bluetteConfiguration"));
+            
+            // FIXME: since key doesn't have dynamic type, we can't properly escape strings.
+            for (final Key<?> key : bluetteConfiguration.getKeys())
+              {
+                final Object value = bluetteConfiguration.getProperty(key);
+                builder.append(String.format("var %s = %s;\n", key.stringValue(), value));
+              }
+          } 
+        catch (NotFoundException e) 
+          {
+            // ok, no configuration
+          }
+        catch (IOException e) 
+          {
+            // ok, no configuration
+          }
+        
+        builder.append("</script>\n");
+        
+        return builder.toString();
       }
   }
