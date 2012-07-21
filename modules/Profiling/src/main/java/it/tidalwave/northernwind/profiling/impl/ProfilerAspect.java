8587cbb8d8278bf25adcfb87e4168aa576d1cd2e
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2012-07-21 23:04:33 +0200

    Partial fix for NW-136.

--- modules/Profiling/src/main/java/it/tidalwave/northernwind/profiling/impl/ProfilerAspect.java
+++ modules/Profiling/src/main/java/it/tidalwave/northernwind/profiling/impl/ProfilerAspect.java
@@ -37,7 +37,8 @@ import lombok.extern.slf4j.Slf4j;
  * @version $Id$
  *
  **********************************************************************************************************************/
-@Aspect @Slf4j
+//@Aspect FIXME: it's not working
+@Slf4j
 public class ProfilerAspect 
   {
     @Around("execution(* it.tidalwave.northernwind.frontend.ui.spi.DefaultSiteViewController.processRequest(..))")
