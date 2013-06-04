9719d9863edbdf23f356a8db8dd048c9a8270b67
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2013-06-04 21:00:52 +0200

    ...

--- modules/Core/src/main/java/it/tidalwave/northernwind/core/model/SiteNode.java
+++ modules/Core/src/main/java/it/tidalwave/northernwind/core/model/SiteNode.java
@@ -52,8 +52,8 @@ public interface SiteNode extends Resource
      *  defined, the local portion of the relative path is used. */
     public static final Key<String> PROPERTY_EXPOSED_URI = new Key<>("exposedUri");
 
-    /** If sets to true, this property makes the {@code SiteNode} to match not only its {@code /relativeUri}, but also
-     *  {@code /relativeUri/something/else}; it is meant for nodes that accept REST path-style params. */
+    /** If sets to true, this property makes the {@code SiteNode} capable to match not only its {@code /relativeUri}, 
+     *  but also {@code /relativeUri/something/else}; it is meant for nodes that accept REST path-style params. */
     // FIXME: must be Boolean
     public static final Key<String> PROPERTY_MANAGES_PATH_PARAMS = new Key<>("managesPathParams");
 
