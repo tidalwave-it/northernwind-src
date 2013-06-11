27dda04b4a4c041e2f27cde0dcb220f045307872
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2013-06-11 17:28:57 +0200

    Added test.

--- modules/CoreImplementation/src/test/java/it/tidalwave/northernwind/core/impl/model/DefaultSiteNodeTest.java
+++ modules/CoreImplementation/src/test/java/it/tidalwave/northernwind/core/impl/model/DefaultSiteNodeTest.java
@@ -30,20 +30,20 @@ package it.tidalwave.northernwind.core.impl.model;
 import it.tidalwave.northernwind.core.model.ModelFactory;
 import it.tidalwave.northernwind.core.model.Resource;
 import it.tidalwave.northernwind.core.model.ResourceFile;
+import it.tidalwave.northernwind.core.model.ResourceProperties;
 import it.tidalwave.northernwind.core.model.Site;
+import it.tidalwave.northernwind.core.model.SiteFinder;
+import it.tidalwave.northernwind.core.model.SiteNode;
 import it.tidalwave.northernwind.frontend.ui.Layout;
 import it.tidalwave.util.Id;
-import java.io.IOException;
-import static org.testng.Assert.*;
+import it.tidalwave.util.NotFoundException;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import static org.mockito.Mockito.*;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import static org.mockito.Mockito.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
 
 /***********************************************************************************************************************
  *
@@ -84,7 +84,8 @@ public class DefaultSiteNodeTest
 
         resource = mock(Resource.class);
         resourceFile = mock(ResourceFile.class);
-        when(resourceFile.getPath()).thenReturn("/resourceFile");
+        when(resourceFile.getPath()).thenReturn("/structure/foo/resourceFile");
+        when(resourceFile.getName()).thenReturn("resourceFile");
         when(resource.getFile()).thenReturn(resourceFile);
         when(modelFactory.createResource(any(ResourceFile.class))).thenReturn(resource);
 
@@ -104,4 +105,54 @@ public class DefaultSiteNodeTest
         assertThat(fixture.resource, sameInstance(resource));
         assertThat(fixture.getLayout(), sameInstance(emptyPlaceHolderLayout));
       }
+
+//    /*******************************************************************************************************************
+//     *
+//     ******************************************************************************************************************/
+//    @Test
+//    public void must_properly_initialize_with_layout()
+//      {
+//        TODO: requires ModelFactory to create a default DefaultLayout -> with a Builder
+//        assertThat(fixture.site, sameInstance(site));
+//        assertThat(fixture.resource, sameInstance(resource));
+//        assertThat(fixture.getLayout(), sameInstance(emptyPlaceHolderLayout));
+//      }
+
+    /*******************************************************************************************************************
+     *
+     ******************************************************************************************************************/
+    @Test
+    public void getRelativeUri_must_return_a_correct_value()
+      throws Exception
+      {
+        final ResourceFile parentResourceFile = mock(ResourceFile.class);
+        final SiteNode parentSiteNode = mock(SiteNode.class);
+        when(parentSiteNode.getRelativeUri()).thenReturn("/parent");
+        when(parentResourceFile.getPath()).thenReturn("/parent");
+        when(resourceFile.getParent()).thenReturn(parentResourceFile);
+
+        final ResourceProperties properties = mock(ResourceProperties.class);
+        when(properties.getProperty(eq(SiteNode.PROPERTY_EXPOSED_URI), anyString())).thenReturn("exposedUri");
+        when(resource.getProperties()).thenReturn(properties);
+
+        final SiteFinder<SiteNode> siteNodeFinder = mock(SiteFinder.class);
+        when(siteNodeFinder.withRelativePath(anyString())).thenReturn(siteNodeFinder); // FIXME: anyString()
+        when(siteNodeFinder.result()).thenReturn(parentSiteNode);
+        when(site.find(eq(SiteNode.class))).thenReturn(siteNodeFinder);
+
+        final String relativeUri = fixture.getRelativeUri();
+
+        assertThat(relativeUri, is("/parent/exposedUri"));
+      }
+
+    // TODO:
+//    /*******************************************************************************************************************
+//     *
+//     ******************************************************************************************************************/
+//    @Test
+//    public void getRelativeUri_must_be_called_only_once()
+//    {
+//
+//    }
+//
   }
