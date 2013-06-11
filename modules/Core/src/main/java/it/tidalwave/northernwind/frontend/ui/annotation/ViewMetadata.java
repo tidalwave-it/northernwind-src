9d02a2f0ce9ea6cc954238b5667f19fdd1c4fd1c
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2013-06-11 15:32:02 +0200

    ...

--- modules/Core/src/main/java/it/tidalwave/northernwind/frontend/ui/annotation/ViewMetadata.java
+++ modules/Core/src/main/java/it/tidalwave/northernwind/frontend/ui/annotation/ViewMetadata.java
@@ -1,27 +1,27 @@
 /*
  * #%L
  * *********************************************************************************************************************
- * 
+ *
  * NorthernWind - lightweight CMS
  * http://northernwind.tidalwave.it - hg clone https://bitbucket.org/tidalwave/northernwind-src
  * %%
  * Copyright (C) 2011 - 2013 Tidalwave s.a.s. (http://tidalwave.it)
  * %%
  * *********************************************************************************************************************
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations under the License.
- * 
+ *
  * *********************************************************************************************************************
- * 
+ *
  * $Id$
- * 
+ *
  * *********************************************************************************************************************
  * #L%
  */
@@ -32,6 +32,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import javax.annotation.Nonnull;
 
 /***********************************************************************************************************************
  *
@@ -48,10 +49,11 @@ public @interface ViewMetadata
   {
     /*******************************************************************************************************************
      *
-     * The type of the component, that will be registered into the 
+     * The type of the component, that will be registered into the
      * {@link it.tidalwave.northernwind.frontend.ui.ViewFactory}.
      *
      ******************************************************************************************************************/
+    @Nonnull
     public String typeUri();
 
     /*******************************************************************************************************************
@@ -59,5 +61,6 @@ public @interface ViewMetadata
      * The class of the Controller for the view.
      *
      ******************************************************************************************************************/
+    @Nonnull
     public Class<?> controlledBy();
   }
