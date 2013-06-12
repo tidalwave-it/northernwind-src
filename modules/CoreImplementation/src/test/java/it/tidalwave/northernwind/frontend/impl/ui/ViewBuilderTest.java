4cc9f272df671093dcdc2fb7a6fef1cd02852f4d
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2013-06-12 23:57:41 +0200

    ...

--- modules/CoreImplementation/src/test/java/it/tidalwave/northernwind/frontend/impl/ui/ViewBuilderTest.java
+++ modules/CoreImplementation/src/test/java/it/tidalwave/northernwind/frontend/impl/ui/ViewBuilderTest.java
@@ -1,9 +1,13 @@
 /*
+ * #%L
+ * *********************************************************************************************************************
  *
- * PROJECT NAME
- * PROJECT COPYRIGHT
- *
- ***********************************************************************************************************************
+ * NorthernWind - lightweight CMS
+ * http://northernwind.tidalwave.it - hg clone https://bitbucket.org/tidalwave/northernwind-src
+ * %%
+ * Copyright (C) 2011 - 2013 Tidalwave s.a.s. (http://tidalwave.it)
+ * %%
+ * *********************************************************************************************************************
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -14,26 +18,27 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations under the License.
  *
- ***********************************************************************************************************************
+ * *********************************************************************************************************************
  *
- * WWW: PROJECT URL
- * SCM: PROJECT SCM
+ * $Id$
  *
+ * *********************************************************************************************************************
+ * #L%
  */
 package it.tidalwave.northernwind.frontend.impl.ui;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import it.tidalwave.util.Id;
 import it.tidalwave.northernwind.core.model.Site;
 import it.tidalwave.northernwind.core.model.SiteNode;
 import it.tidalwave.northernwind.core.model.SiteProvider;
 import it.tidalwave.northernwind.frontend.ui.ViewFactory.ViewAndController;
-import it.tidalwave.util.Id;
 import lombok.RequiredArgsConstructor;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.CoreMatchers.*;
 import static org.mockito.Mockito.*;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.testng.annotations.BeforeMethod;
 
 /***********************************************************************************************************************
  *
