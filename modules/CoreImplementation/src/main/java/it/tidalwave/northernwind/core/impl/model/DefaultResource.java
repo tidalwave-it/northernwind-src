c3ad5968a86001eea8579b31eed8bff97bde32c8
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2013-06-15 19:21:41 +0200

    Refactored.

--- modules/CoreImplementation/src/main/java/it/tidalwave/northernwind/core/impl/model/DefaultResource.java
+++ modules/CoreImplementation/src/main/java/it/tidalwave/northernwind/core/impl/model/DefaultResource.java
@@ -30,17 +30,13 @@ package it.tidalwave.northernwind.core.impl.model;
 import javax.annotation.Nonnull;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
-import javax.inject.Provider;
-import java.util.Locale;
 import java.io.InputStream;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import org.springframework.beans.factory.annotation.Configurable;
 import it.tidalwave.util.Id;
 import it.tidalwave.util.Key;
 import it.tidalwave.util.NotFoundException;
 import it.tidalwave.northernwind.core.model.ModelFactory;
-import it.tidalwave.northernwind.core.model.RequestLocaleManager;
 import it.tidalwave.northernwind.core.model.Resource;
 import it.tidalwave.northernwind.core.model.ResourceFile;
 import it.tidalwave.northernwind.core.model.ResourceProperties;
@@ -52,6 +48,8 @@ import static it.tidalwave.role.Unmarshallable.Unmarshallable;
 
 /***********************************************************************************************************************
  *
+ * The default implementation for {@link Resource}.
+ *
  * @author  Fabrizio Giudici
  * @version $Id$
  *
@@ -61,12 +59,6 @@ import static it.tidalwave.role.Unmarshallable.Unmarshallable;
   {
     @Inject @Nonnull
     private ModelFactory modelFactory;
-
-    @Inject @Nonnull
-    private RequestLocaleManager localeRequestManager;
-
-    @Inject @Nonnull
-    private Provider<FilterSetExpander> macroExpander;
 
     @Inject @Nonnull
     private InheritanceHelper inheritanceHelper;
@@ -80,19 +72,7 @@ import static it.tidalwave.role.Unmarshallable.Unmarshallable;
     @Getter
     private boolean placeHolder;
 
-    /*******************************************************************************************************************
-     *
-     *
-     ******************************************************************************************************************/
-    private ResourceProperties.PropertyResolver propertyResolver = new ResourceProperties.PropertyResolver()
-      {
-        @Override @SuppressWarnings("unchecked")
-        public <Type> Type resolveProperty (final @Nonnull Id propertyGroupId, final @Nonnull Key<Type> key)
-          throws NotFoundException, IOException
-          {
-            return (Type)getFileBasedProperty(key.stringValue()); // FIXME: use also Id for SiteNode?
-          }
-      };
+    private final ResourceProperties.PropertyResolver propertyResolver;
 
     /*******************************************************************************************************************
      *
@@ -101,6 +81,7 @@ import static it.tidalwave.role.Unmarshallable.Unmarshallable;
     public DefaultResource (final @Nonnull ResourceFile file)
       {
         this.file = file;
+        propertyResolver = new HtmlResourcePropertyResolver(file);
       }
 
     /*******************************************************************************************************************
@@ -112,31 +93,6 @@ import static it.tidalwave.role.Unmarshallable.Unmarshallable;
     public ResourceProperties getPropertyGroup (final @Nonnull Id id)
       {
         return properties.getGroup(id);
-      }
-
-    /*******************************************************************************************************************
-     *
-     *
-     *
-     ******************************************************************************************************************/
-    @Nonnull
-    private String getFileBasedProperty (final @Nonnull String propertyName)
-      throws NotFoundException, IOException
-      {
-        log.trace("getFileBasedProperty({})", propertyName);
-
-        final ResourceFile propertyFile = findLocalizedFile(propertyName);
-        log.trace(">>>> reading from {}", propertyFile.getPath());
-        final String charset = propertyFile.getMimeType().equals("application/xhtml+xml") ? "UTF-8" : Charset.defaultCharset().name();
-
-        try
-          {
-            return macroExpander.get().filter(propertyFile.asText(charset), propertyFile.getMimeType());
-          }
-        catch (RuntimeException e) // FIXME: introduce a FilterException
-          {
-            throw new IOException(e);
-          }
       }
 
     /*******************************************************************************************************************
@@ -157,7 +113,8 @@ import static it.tidalwave.role.Unmarshallable.Unmarshallable;
             log.trace(">>>> reading properties from {}...", propertyFile.getPath().asString());
             @Cleanup final InputStream is = propertyFile.getInputStream();
             final ResourceProperties tempProperties =
-                modelFactory.createProperties().withPropertyResolver(propertyResolver).build().as(Unmarshallable).unmarshal(is);
+                modelFactory.createProperties().build().as(Unmarshallable).unmarshal(is);
+//                modelFactory.createProperties().withPropertyResolver(propertyResolver).build().as(Unmarshallable).unmarshal(is);
             log.trace(">>>>>>>> read properties: {}", tempProperties);
             properties = properties.merged(tempProperties);
             tmpPlaceHolder &= !propertyFile.getParent().equals(file);
@@ -174,43 +131,7 @@ import static it.tidalwave.role.Unmarshallable.Unmarshallable;
 
     /*******************************************************************************************************************
      *
-     *
-     ******************************************************************************************************************/
-    @Nonnull
-    private ResourceFile findLocalizedFile (final @Nonnull String fileName)
-      throws NotFoundException
-      {
-        log.trace("findLocalizedFile({})", fileName);
-        ResourceFile localizedFile = null;
-        final StringBuilder fileNamesNotFound = new StringBuilder();
-        String separator = "";
-
-        for (final Locale locale : localeRequestManager.getLocales())
-          {
-            final String localizedFileName = fileName.replace(".", "_" + locale.getLanguage() + ".");
-            localizedFile = file.getChildByName(localizedFileName);
-
-            if ((localizedFile == null) && localizedFileName.endsWith(".xhtml"))
-              {
-                localizedFile = file.getChildByName(localizedFileName.replaceAll("\\.xhtml$", ".html"));
-              }
-
-            if (localizedFile != null)
-              {
-                break;
-              }
-
-            fileNamesNotFound.append(separator);
-            fileNamesNotFound.append(localizedFileName);
-            separator = ",";
-          }
-
-        return NotFoundException.throwWhenNull(localizedFile,
-                    String.format("findLocalizedFile(): %s/{%s}", file.getPath().asString(), fileNamesNotFound));
-      }
-
-    /*******************************************************************************************************************
-     *
+     * FIXME: move to ResourceProperties!
      *
      ******************************************************************************************************************/
     private void logProperties (final @Nonnull String indent ,final @Nonnull ResourceProperties properties)
