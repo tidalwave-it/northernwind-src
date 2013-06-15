7bb1f3b71feaa45e4a230a118e270807392be044
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2013-06-16 00:45:19 +0200

    Fixed broken test.

--- modules/CoreImplementation/src/test/java/it/tidalwave/northernwind/core/impl/model/DefaultSiteNodeTest.java
+++ modules/CoreImplementation/src/test/java/it/tidalwave/northernwind/core/impl/model/DefaultSiteNodeTest.java
@@ -32,6 +32,7 @@ import javax.annotation.Nonnull;
 import java.io.IOException;
 import it.tidalwave.util.NotFoundException;
 import it.tidalwave.northernwind.core.model.ModelFactory;
+import it.tidalwave.northernwind.core.model.RequestLocaleManager;
 import it.tidalwave.northernwind.core.model.ResourcePath;
 import it.tidalwave.northernwind.core.model.Resource;
 import it.tidalwave.northernwind.core.model.ResourceFile;
@@ -39,6 +40,8 @@ import it.tidalwave.northernwind.core.model.ResourceProperties;
 import it.tidalwave.northernwind.core.model.SiteFinder;
 import it.tidalwave.northernwind.core.model.SiteNode;
 import it.tidalwave.northernwind.frontend.ui.Layout;
+import java.util.Arrays;
+import java.util.Locale;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -72,6 +75,8 @@ public class DefaultSiteNodeTest
 
     private InheritanceHelper inheritanceHelper;
 
+    private RequestLocaleManager requestLocaleManager;
+
     private Layout emptyPlaceHolderLayout;
 
     /*******************************************************************************************************************
@@ -85,11 +90,13 @@ public class DefaultSiteNodeTest
         site = context.getBean(InternalSite.class);
         modelFactory = context.getBean(ModelFactory.class);
         inheritanceHelper = context.getBean(InheritanceHelper.class);
+        requestLocaleManager = context.getBean(RequestLocaleManager.class);
 
         resource = mock(Resource.class);
         resourceFile = MockResourceFile.folder("/structure/foo/resourceFile");
         when(resource.getFile()).thenReturn(resourceFile);
         when(modelFactory.createResource(any(ResourceFile.class))).thenReturn(resource);
+        when(requestLocaleManager.getLocales()).thenReturn(Arrays.asList(Locale.ENGLISH));
 
         final ResourceFile nodeFolder = MockResourceFile.folder("structure");
         when(site.getNodeFolder()).thenReturn(nodeFolder);
