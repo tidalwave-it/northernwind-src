0366760333be16832d36e98fd5d5f256284dce93
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2013-07-12 22:54:22 +0200

    Fix for NW-191.

--- modules/Core/src/main/java/it/tidalwave/northernwind/core/model/spi/ModelFactorySupport.java
+++ modules/Core/src/main/java/it/tidalwave/northernwind/core/model/spi/ModelFactorySupport.java
@@ -25,14 +25,9 @@
  * *********************************************************************************************************************
  * #L%
  */
-package it.tidalwave.northernwind.core.impl.model;
+package it.tidalwave.northernwind.core.model.spi;
 
 import javax.annotation.Nonnull;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
 import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import it.tidalwave.util.NotFoundException;
@@ -45,21 +40,18 @@ import it.tidalwave.northernwind.core.model.ResourceFile;
 import it.tidalwave.northernwind.core.model.ResourceProperties;
 import it.tidalwave.northernwind.core.model.Site;
 import it.tidalwave.northernwind.core.model.SiteNode;
-import it.tidalwave.northernwind.core.model.spi.ModelFactorySupport;
 import it.tidalwave.northernwind.frontend.ui.Layout;
-import it.tidalwave.northernwind.frontend.impl.ui.DefaultLayout;
-import lombok.ToString;
 
 /***********************************************************************************************************************
- *
- * The default implementation of {@link ModelFactory}.
  *
  * @author  Fabrizio Giudici
  * @version $Id$
  *
  **********************************************************************************************************************/
-@ToString
-public class DefaultModelFactory extends ModelFactorySupport
+public class ModelFactorySupport implements ModelFactory,
+                                            ResourceProperties.Builder.CallBack,
+                                            Site.Builder.CallBack,
+                                            Layout.Builder.CallBack
   {
     /*******************************************************************************************************************
      *
@@ -69,7 +61,7 @@ public class DefaultModelFactory extends ModelFactorySupport
     @Override @Nonnull
     public Resource createResource (final @Nonnull ResourceFile file)
       {
-        return new DefaultResource(file);
+        throw new UnsupportedOperationException("Not supported.");
       }
 
     /*******************************************************************************************************************
@@ -78,20 +70,9 @@ public class DefaultModelFactory extends ModelFactorySupport
      *
      ******************************************************************************************************************/
     @Override @Nonnull
-    public Content createContent (final @Nonnull ResourceFile folder)
+    public Content createContent (final @Nonnull ResourceFile file)
       {
-        return new DefaultContent(folder);
-      }
-
-    /*******************************************************************************************************************
-     *
-     * {@inheritDoc}
-     *
-     ******************************************************************************************************************/
-    @Override @Nonnull
-    public Media createMedia (final @Nonnull ResourceFile file)
-      {
-        return new DefaultMedia(file);
+        throw new UnsupportedOperationException("Not supported.");
       }
 
     /*******************************************************************************************************************
@@ -103,7 +84,7 @@ public class DefaultModelFactory extends ModelFactorySupport
     public SiteNode createSiteNode (final @Nonnull Site site, final @Nonnull ResourceFile folder)
       throws IOException, NotFoundException
       {
-        return new DefaultSiteNode((DefaultSite)site, folder);
+        throw new UnsupportedOperationException("Not supported.");
       }
 
     /*******************************************************************************************************************
@@ -112,9 +93,9 @@ public class DefaultModelFactory extends ModelFactorySupport
      *
      ******************************************************************************************************************/
     @Override @Nonnull
-    public Layout build (final @Nonnull Layout.Builder builder)
+    public Media createMedia (final @Nonnull ResourceFile rf)
       {
-        return new DefaultLayout(builder);
+        throw new UnsupportedOperationException("Not supported.");
       }
 
     /*******************************************************************************************************************
@@ -123,9 +104,9 @@ public class DefaultModelFactory extends ModelFactorySupport
      *
      ******************************************************************************************************************/
     @Override @Nonnull
-    public DefaultRequest createRequest()
+    public Request createRequest()
       {
-        return new DefaultRequest("", "", "", new HashMap<String, List<String>>(), new ArrayList<Locale>());
+        throw new UnsupportedOperationException("Not supported.");
       }
 
     /*******************************************************************************************************************
@@ -133,15 +114,43 @@ public class DefaultModelFactory extends ModelFactorySupport
      * {@inheritDoc}
      *
      ******************************************************************************************************************/
-    @Override @Nonnull @SuppressWarnings("unchecked")
-    public Request createRequestFrom (final @Nonnull HttpServletRequest httpServletRequest)
+    @Override @Nonnull
+    public Request createRequestFrom (final @Nonnull HttpServletRequest hsr)
       {
-        String relativeUri = httpServletRequest.getRequestURI().substring(httpServletRequest.getContextPath().length());
-        relativeUri = relativeUri.equals("") ? "/" : relativeUri;
-        return createRequest().withBaseUrl(getBaseUrl(httpServletRequest))
-                              .withRelativeUri(relativeUri)
-                              .withParameterMap(httpServletRequest.getParameterMap())
-                              .withPreferredLocales(Collections.list(httpServletRequest.getLocales()));
+        throw new UnsupportedOperationException("Not supported.");
+      }
+
+    /*******************************************************************************************************************
+     *
+     * {@inheritDoc}
+     *
+     ******************************************************************************************************************/
+    @Override @Nonnull
+    public final Layout.Builder createLayout()
+      {
+        return new Layout.Builder().withCallBack(this);
+      }
+
+    /*******************************************************************************************************************
+     *
+     * {@inheritDoc}
+     *
+     ******************************************************************************************************************/
+    @Override @Nonnull
+    public final ResourceProperties.Builder createProperties()
+      {
+        return new ResourceProperties.Builder().withCallBack(this);
+      }
+
+    /*******************************************************************************************************************
+     *
+     * {@inheritDoc}
+     *
+     ******************************************************************************************************************/
+    @Override @Nonnull
+    public final Site.Builder createSite()
+      {
+        return new Site.Builder().withCallBack(this);
       }
 
     /*******************************************************************************************************************
@@ -152,8 +161,7 @@ public class DefaultModelFactory extends ModelFactorySupport
     @Override @Nonnull
     public ResourceProperties build (final @Nonnull ResourceProperties.Builder builder)
       {
-        return new DefaultResourceProperties(builder);
-//        return new DefaultResourceProperties(id, DefaultResourceProperties.PropertyResolver.DEFAULT);
+        throw new UnsupportedOperationException("Not supported.");
       }
 
     /*******************************************************************************************************************
@@ -164,17 +172,17 @@ public class DefaultModelFactory extends ModelFactorySupport
     @Override @Nonnull
     public Site build (final @Nonnull Site.Builder builder)
       {
-        return new DefaultSite(builder);
+        throw new UnsupportedOperationException("Not supported.");
       }
 
     /*******************************************************************************************************************
      *
+     * {@inheritDoc}
      *
      ******************************************************************************************************************/
-    @Nonnull
-    private static String getBaseUrl (final @Nonnull HttpServletRequest httpServletRequest)
+    @Override @Nonnull
+    public Layout build (final @Nonnull Layout.Builder builder)
       {
-        return httpServletRequest.getRequestURL().toString().replaceAll(":.*", "")
-                + "://" + httpServletRequest.getHeader("Host");
+        throw new UnsupportedOperationException("Not supported.");
       }
   }
