aa3eaf883d4f61a2926b8bba3b5367379f8ee844
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2014-05-15 23:25:22 +0200

    Merged.

---
+++ modules/HtmlTemplateComponents/src/main/java/it/tidalwave/northernwind/frontend/ui/component/htmltextwithtitle/htmltemplate/HtmlTemplateHtmlTextWithTitleView.java
@@ -0,0 +1,76 @@
+/*
+ * #%L
+ * *********************************************************************************************************************
+ * 
+ * NorthernWind - lightweight CMS
+ * http://northernwind.tidalwave.it - hg clone https://bitbucket.org/tidalwave/northernwind-src
+ * %%
+ * Copyright (C) 2011 - 2014 Tidalwave s.a.s. (http://tidalwave.it)
+ * %%
+ * *********************************************************************************************************************
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ * 
+ * *********************************************************************************************************************
+ * 
+ * $Id$
+ * 
+ * *********************************************************************************************************************
+ * #L%
+ */
+package it.tidalwave.northernwind.frontend.ui.component.htmltextwithtitle.htmltemplate;
+
+import javax.annotation.Nonnull;
+import it.tidalwave.util.Id;
+import it.tidalwave.northernwind.frontend.ui.annotation.ViewMetadata;
+import it.tidalwave.northernwind.frontend.ui.component.htmltextwithtitle.HtmlTextWithTitleView;
+import it.tidalwave.northernwind.frontend.ui.component.htmltextwithtitle.DefaultHtmlTextWithTitleViewController;
+import it.tidalwave.northernwind.frontend.ui.component.htmltemplate.HtmlHolder;
+import lombok.Getter;
+
+/***********************************************************************************************************************
+ *
+ * The HtmlTemplateimplementation of {@link HtmlTextWithTitleView}.
+ * 
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+@ViewMetadata(typeUri="http://northernwind.tidalwave.it/component/HtmlTextWithTitle/#v1.0", 
+              controlledBy=DefaultHtmlTextWithTitleViewController.class)
+public class HtmlTemplateHtmlTextWithTitleView extends HtmlHolder implements HtmlTextWithTitleView
+  {
+    @Getter @Nonnull
+    private final Id id;
+    
+    /*******************************************************************************************************************
+     *
+     * Creates an instance with the given id.
+     * 
+     * @param  id  the id
+     *
+     ******************************************************************************************************************/
+    public HtmlTemplateHtmlTextWithTitleView (final @Nonnull Id id) 
+      {
+        super(id);
+        this.id = id;
+      }
+    
+    /*******************************************************************************************************************
+     *
+     * {@inheritDoc}
+     *
+     ******************************************************************************************************************/
+    @Override
+    public void setText (final @Nonnull String text)
+      {
+        setContent(text);
+      }
+  }
---
+++ modules/frontend/Commons/src/main/java/it/tidalwave/northernwind/frontend/util/BootLogger.java
@@ -0,0 +1,69 @@
+/*
+ * #%L
+ * *********************************************************************************************************************
+ * 
+ * NorthernWind - lightweight CMS
+ * http://northernwind.tidalwave.it - hg clone https://bitbucket.org/tidalwave/northernwind-src
+ * %%
+ * Copyright (C) 2011 - 2014 Tidalwave s.a.s. (http://tidalwave.it)
+ * %%
+ * *********************************************************************************************************************
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ * 
+ * *********************************************************************************************************************
+ * 
+ * $Id$
+ * 
+ * *********************************************************************************************************************
+ * #L%
+ */
+package it.tidalwave.northernwind.frontend.util;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.annotation.Nonnull;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/***********************************************************************************************************************
+ *
+ * We can't log to the real thing, since we first need to compute the path of the logging file. Logging to the real
+ * thing would instantiate the logging facility before we have a chance to configure it.
+ *
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class BootLogger
+  {
+    private static final StringBuilder BUILDER = new StringBuilder();
+
+    public static synchronized void log (final @Nonnull String string)
+      {
+        BUILDER.append(string).append("\n");
+      }
+
+    public static void log (final @Nonnull Throwable t)
+      {
+        final StringWriter sw = new StringWriter();
+        final PrintWriter pw = new PrintWriter(sw);
+        t.printStackTrace(pw);
+        pw.flush();
+        log(t.toString() + "\n" + sw.toString());
+      }
+
+    @Nonnull
+    public static String getLogContent()
+      {
+        return BUILDER.toString();
+      }
+  }
