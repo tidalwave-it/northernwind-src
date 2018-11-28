255ced6258e253c36cdb0aeef2a73f49c2406606
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2018-11-28 11:58:41 +0100

    Refactored.

--- modules/HtmlTemplateComponents/src/test/java/it/tidalwave/northernwind/frontend/ui/component/blog/htmltemplate/HtmlTemplateBlogViewControllerTest.java
+++ modules/HtmlTemplateComponents/src/test/java/it/tidalwave/northernwind/frontend/ui/component/blog/htmltemplate/HtmlTemplateBlogViewControllerTest.java
@@ -30,12 +30,12 @@ package it.tidalwave.northernwind.frontend.ui.component.blog.htmltemplate;
 import javax.annotation.Nonnull;
 import java.util.Arrays;
 import java.util.Locale;
+import java.util.Optional;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
-import java.util.Optional;
 import it.tidalwave.util.LocalizedDateTimeFormatters;
 import it.tidalwave.util.Id;
 import it.tidalwave.util.Key;
@@ -56,8 +56,6 @@ import static it.tidalwave.northernwind.frontend.ui.component.blog.htmltemplate.
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.*;
 import static org.mockito.Mockito.*;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 
 /***********************************************************************************************************************
  *
@@ -123,7 +121,7 @@ public class HtmlTemplateBlogViewControllerTest
                                                final @Nonnull Optional<String> dateFormat,
                                                final @Nonnull ZonedDateTime dateTime,
                                                final @Nonnull Optional<String> timeZone,
-                                               final @Nonnull String expectedRendering)
+                                               final @Nonnull String expectedDTRendering)
       throws Exception
       {
         // given
@@ -140,7 +138,8 @@ public class HtmlTemplateBlogViewControllerTest
         final StringBuilder builder = new StringBuilder();
         underTest.renderDate(builder, dateTime);
         // then
-        assertThat(builder.toString(), is(expectedRendering));
+        final String expectedValue = String.format("<span class='nw-publishDate'>%s</span>\n", expectedDTRendering);
+        assertThat(builder.toString(), is(expectedValue));
       }
 
     /*******************************************************************************************************************
@@ -195,92 +194,98 @@ public class HtmlTemplateBlogViewControllerTest
       {
         final ZonedDateTime dt = Instant.ofEpochMilli(1344353463985L).atZone(ZoneId.of("GMT"));
 
-        final String pattern = "EEEEEEEEEE, MMMMMM d, yyyy";
-        final Optional<String> tzNone  = Optional.empty();
-        final Optional<String> tzGMT   = Optional.of("GMT");
-        final Optional<String> tzCET   = Optional.of("CET");
-        final Optional<String> tzPDT   = Optional.of("America/Los_Angeles");
-        final Optional<String> tzGMT10 = Optional.of("GMT+10");
+        final Optional<String> noPattern   = Optional.empty();
+        final Optional<String> pattern     = Optional.of("EEEEEEEEEE, MMMMMM d, yyyy");
+        final Optional<String> shortStyle  = Optional.of("S-");
+        final Optional<String> mediumStyle = Optional.of("M-");
+        final Optional<String> longStyle   = Optional.of("L-");
+        final Optional<String> fullStyle   = Optional.of("F-");
+
+        final Optional<String> tzNone      = Optional.empty();
+        final Optional<String> tzGMT       = Optional.of("GMT");
+        final Optional<String> tzCET       = Optional.of("CET");
+        final Optional<String> tzPDT       = Optional.of("America/Los_Angeles");
+        final Optional<String> tzGMT10     = Optional.of("GMT+10");
 
         return new Object[][]
           {
-           // loc.  date format           value   timezone    expected value
-            { "en", Optional.empty(),     dt,     tzNone,     "<span class='nw-publishDate'>Tuesday, August 7, 2012 5:31:03 PM CEST</span>\n"},
-            { "en", Optional.empty(),     dt,     tzGMT,      "<span class='nw-publishDate'>Tuesday, August 7, 2012 3:31:03 PM GMT</span>\n"},
-            { "en", Optional.empty(),     dt,     tzCET,      "<span class='nw-publishDate'>Tuesday, August 7, 2012 5:31:03 PM CEST</span>\n"},
-            { "en", Optional.empty(),     dt,     tzPDT,      "<span class='nw-publishDate'>Tuesday, August 7, 2012 8:31:03 AM PDT</span>\n"},
-            { "en", Optional.empty(),     dt,     tzGMT10,    "<span class='nw-publishDate'>Wednesday, August 8, 2012 1:31:03 AM GMT+10:00</span>\n"},
+           // loc.  format         value   timezone    expected value
+            { "en", noPattern,     dt,     tzNone,     "Tuesday, August 7, 2012 5:31:03 PM CEST"},
+            { "en", noPattern,     dt,     tzGMT,      "Tuesday, August 7, 2012 3:31:03 PM GMT"},
+            { "en", noPattern,     dt,     tzCET,      "Tuesday, August 7, 2012 5:31:03 PM CEST"},
+            { "en", noPattern,     dt,     tzPDT,      "Tuesday, August 7, 2012 8:31:03 AM PDT"},
+            { "en", noPattern,     dt,     tzGMT10,    "Wednesday, August 8, 2012 1:31:03 AM GMT+10:00"},
 
-            { "it", Optional.empty(),     dt,     tzNone,     "<span class='nw-publishDate'>marted\u00ec 7 agosto 2012 17:31:03 CEST</span>\n"},
-            { "it", Optional.empty(),     dt,     tzGMT,      "<span class='nw-publishDate'>marted\u00ec 7 agosto 2012 15:31:03 GMT</span>\n"},
-            { "it", Optional.empty(),     dt,     tzCET,      "<span class='nw-publishDate'>marted\u00ec 7 agosto 2012 17:31:03 CEST</span>\n"},
-            { "it", Optional.empty(),     dt,     tzPDT,      "<span class='nw-publishDate'>marted\u00ec 7 agosto 2012 8:31:03 PDT</span>\n"},
-            { "it", Optional.empty(),     dt,     tzGMT10,    "<span class='nw-publishDate'>mercoled\u00ec 8 agosto 2012 1:31:03 GMT+10:00</span>\n"},
-
-
-            { "en", Optional.of(pattern), dt,     tzNone,     "<span class='nw-publishDate'>Tuesday, August 7, 2012</span>\n"},
-            { "en", Optional.of(pattern), dt,     tzGMT,      "<span class='nw-publishDate'>Tuesday, August 7, 2012</span>\n"},
-            { "en", Optional.of(pattern), dt,     tzCET,      "<span class='nw-publishDate'>Tuesday, August 7, 2012</span>\n"},
-            { "en", Optional.of(pattern), dt,     tzPDT,      "<span class='nw-publishDate'>Tuesday, August 7, 2012</span>\n"},
-            { "en", Optional.of(pattern), dt,     tzGMT10,    "<span class='nw-publishDate'>Wednesday, August 8, 2012</span>\n"},
-
-            { "it", Optional.of(pattern), dt,     tzNone,     "<span class='nw-publishDate'>marted\u00ec, agosto 7, 2012</span>\n"}, // FIXME: should be '7 agosto'
-            { "it", Optional.of(pattern), dt,     tzGMT,      "<span class='nw-publishDate'>marted\u00ec, agosto 7, 2012</span>\n"},
-            { "it", Optional.of(pattern), dt,     tzCET,      "<span class='nw-publishDate'>marted\u00ec, agosto 7, 2012</span>\n"},
-            { "it", Optional.of(pattern), dt,     tzPDT,      "<span class='nw-publishDate'>marted\u00ec, agosto 7, 2012</span>\n"},
-            { "it", Optional.of(pattern), dt,     tzGMT10,    "<span class='nw-publishDate'>mercoled\u00ec, agosto 8, 2012</span>\n"},
+            { "it", noPattern,     dt,     tzNone,     "marted\u00ec 7 agosto 2012 17:31:03 CEST"},
+            { "it", noPattern,     dt,     tzGMT,      "marted\u00ec 7 agosto 2012 15:31:03 GMT"},
+            { "it", noPattern,     dt,     tzCET,      "marted\u00ec 7 agosto 2012 17:31:03 CEST"},
+            { "it", noPattern,     dt,     tzPDT,      "marted\u00ec 7 agosto 2012 8:31:03 PDT"},
+            { "it", noPattern,     dt,     tzGMT10,    "mercoled\u00ec 8 agosto 2012 1:31:03 GMT+10:00"},
 
 
-            { "en", Optional.of("S-"),    dt,     tzNone,     "<span class='nw-publishDate'>8/7/12 5:31 PM</span>\n"},
-            { "en", Optional.of("S-"),    dt,     tzGMT,      "<span class='nw-publishDate'>8/7/12 3:31 PM</span>\n"},
-            { "en", Optional.of("S-"),    dt,     tzCET,      "<span class='nw-publishDate'>8/7/12 5:31 PM</span>\n"},
-            { "en", Optional.of("S-"),    dt,     tzPDT,      "<span class='nw-publishDate'>8/7/12 8:31 AM</span>\n"},
-            { "en", Optional.of("S-"),    dt,     tzGMT10,    "<span class='nw-publishDate'>8/8/12 1:31 AM</span>\n"},
+            { "en", pattern,       dt,     tzNone,     "Tuesday, August 7, 2012"},
+            { "en", pattern,       dt,     tzGMT,      "Tuesday, August 7, 2012"},
+            { "en", pattern,       dt,     tzCET,      "Tuesday, August 7, 2012"},
+            { "en", pattern,       dt,     tzPDT,      "Tuesday, August 7, 2012"},
+            { "en", pattern,       dt,     tzGMT10,    "Wednesday, August 8, 2012"},
 
-            { "it", Optional.of("S-"),    dt,     tzNone,     "<span class='nw-publishDate'>07/08/12 17:31</span>\n"},
-            { "it", Optional.of("S-"),    dt,     tzGMT,      "<span class='nw-publishDate'>07/08/12 15:31</span>\n"},
-            { "it", Optional.of("S-"),    dt,     tzCET,      "<span class='nw-publishDate'>07/08/12 17:31</span>\n"},
-            { "it", Optional.of("S-"),    dt,     tzPDT,      "<span class='nw-publishDate'>07/08/12 8:31</span>\n"},
-            { "it", Optional.of("S-"),    dt,     tzGMT10,    "<span class='nw-publishDate'>08/08/12 1:31</span>\n"},
-
-
-            { "en", Optional.of("M-"),    dt,     tzNone,     "<span class='nw-publishDate'>Aug 7, 2012 5:31 PM</span>\n"},
-            { "en", Optional.of("M-"),    dt,     tzGMT,      "<span class='nw-publishDate'>Aug 7, 2012 3:31 PM</span>\n"},
-            { "en", Optional.of("M-"),    dt,     tzCET,      "<span class='nw-publishDate'>Aug 7, 2012 5:31 PM</span>\n"},
-            { "en", Optional.of("M-"),    dt,     tzPDT,      "<span class='nw-publishDate'>Aug 7, 2012 8:31 AM</span>\n"},
-            { "en", Optional.of("M-"),    dt,     tzGMT10,    "<span class='nw-publishDate'>Aug 8, 2012 1:31 AM</span>\n"},
-
-            { "it", Optional.of("M-"),    dt,     tzNone,     "<span class='nw-publishDate'>7-ago-2012 17:31</span>\n"},
-            { "it", Optional.of("M-"),    dt,     tzGMT,      "<span class='nw-publishDate'>7-ago-2012 15:31</span>\n"},
-            { "it", Optional.of("M-"),    dt,     tzCET,      "<span class='nw-publishDate'>7-ago-2012 17:31</span>\n"},
-            { "it", Optional.of("M-"),    dt,     tzPDT,      "<span class='nw-publishDate'>7-ago-2012 8:31</span>\n"},
-            { "it", Optional.of("M-"),    dt,     tzGMT10,    "<span class='nw-publishDate'>8-ago-2012 1:31</span>\n"},
+            { "it", pattern,       dt,     tzNone,     "marted\u00ec, agosto 7, 2012"}, // FIXME: should be '7 agosto'
+            { "it", pattern,       dt,     tzGMT,      "marted\u00ec, agosto 7, 2012"},
+            { "it", pattern,       dt,     tzCET,      "marted\u00ec, agosto 7, 2012"},
+            { "it", pattern,       dt,     tzPDT,      "marted\u00ec, agosto 7, 2012"},
+            { "it", pattern,       dt,     tzGMT10,    "mercoled\u00ec, agosto 8, 2012"},
 
 
-            { "en", Optional.of("L-"),    dt,     tzNone,     "<span class='nw-publishDate'>August 7, 2012 5:31:03 PM</span>\n"},
-            { "en", Optional.of("L-"),    dt,     tzGMT,      "<span class='nw-publishDate'>August 7, 2012 3:31:03 PM</span>\n"},
-            { "en", Optional.of("L-"),    dt,     tzCET,      "<span class='nw-publishDate'>August 7, 2012 5:31:03 PM</span>\n"},
-            { "en", Optional.of("L-"),    dt,     tzPDT,      "<span class='nw-publishDate'>August 7, 2012 8:31:03 AM</span>\n"},
-            { "en", Optional.of("L-"),    dt,     tzGMT10,    "<span class='nw-publishDate'>August 8, 2012 1:31:03 AM</span>\n"},
+            { "en", shortStyle,    dt,     tzNone,     "8/7/12 5:31 PM"},
+            { "en", shortStyle,    dt,     tzGMT,      "8/7/12 3:31 PM"},
+            { "en", shortStyle,    dt,     tzCET,      "8/7/12 5:31 PM"},
+            { "en", shortStyle,    dt,     tzPDT,      "8/7/12 8:31 AM"},
+            { "en", shortStyle,    dt,     tzGMT10,    "8/8/12 1:31 AM"},
 
-            { "it", Optional.of("L-"),    dt,     tzNone,     "<span class='nw-publishDate'>7 agosto 2012 17:31:03</span>\n"},
-            { "it", Optional.of("L-"),    dt,     tzGMT,      "<span class='nw-publishDate'>7 agosto 2012 15:31:03</span>\n"},
-            { "it", Optional.of("L-"),    dt,     tzCET,      "<span class='nw-publishDate'>7 agosto 2012 17:31:03</span>\n"},
-            { "it", Optional.of("L-"),    dt,     tzPDT,      "<span class='nw-publishDate'>7 agosto 2012 8:31:03</span>\n"},
-            { "it", Optional.of("L-"),    dt,     tzGMT10,    "<span class='nw-publishDate'>8 agosto 2012 1:31:03</span>\n"},
+            { "it", shortStyle,    dt,     tzNone,     "07/08/12 17:31"},
+            { "it", shortStyle,    dt,     tzGMT,      "07/08/12 15:31"},
+            { "it", shortStyle,    dt,     tzCET,      "07/08/12 17:31"},
+            { "it", shortStyle,    dt,     tzPDT,      "07/08/12 8:31"},
+            { "it", shortStyle,    dt,     tzGMT10,    "08/08/12 1:31"},
 
 
-            { "en", Optional.of("F-"),    dt,     tzNone,     "<span class='nw-publishDate'>Tuesday, August 7, 2012 5:31:03 PM CEST</span>\n"},
-            { "en", Optional.of("F-"),    dt,     tzGMT,      "<span class='nw-publishDate'>Tuesday, August 7, 2012 3:31:03 PM GMT</span>\n"},
-            { "en", Optional.of("F-"),    dt,     tzCET,      "<span class='nw-publishDate'>Tuesday, August 7, 2012 5:31:03 PM CEST</span>\n"},
-            { "en", Optional.of("F-"),    dt,     tzPDT,      "<span class='nw-publishDate'>Tuesday, August 7, 2012 8:31:03 AM PDT</span>\n"},
-            { "en", Optional.of("F-"),    dt,     tzGMT10,    "<span class='nw-publishDate'>Wednesday, August 8, 2012 1:31:03 AM GMT+10:00</span>\n"},
+            { "en", mediumStyle,   dt,     tzNone,     "Aug 7, 2012 5:31 PM"},
+            { "en", mediumStyle,   dt,     tzGMT,      "Aug 7, 2012 3:31 PM"},
+            { "en", mediumStyle,   dt,     tzCET,      "Aug 7, 2012 5:31 PM"},
+            { "en", mediumStyle,   dt,     tzPDT,      "Aug 7, 2012 8:31 AM"},
+            { "en", mediumStyle,   dt,     tzGMT10,    "Aug 8, 2012 1:31 AM"},
 
-            { "it", Optional.of("F-"),    dt,     tzNone,     "<span class='nw-publishDate'>marted\u00ec 7 agosto 2012 17:31:03 CEST</span>\n"},
-            { "it", Optional.of("F-"),    dt,     tzGMT,      "<span class='nw-publishDate'>marted\u00ec 7 agosto 2012 15:31:03 GMT</span>\n"},
-            { "it", Optional.of("F-"),    dt,     tzCET,      "<span class='nw-publishDate'>marted\u00ec 7 agosto 2012 17:31:03 CEST</span>\n"},
-            { "it", Optional.of("F-"),    dt,     tzPDT,      "<span class='nw-publishDate'>marted\u00ec 7 agosto 2012 8:31:03 PDT</span>\n"},
-            { "it", Optional.of("F-"),    dt,     tzGMT10,    "<span class='nw-publishDate'>mercoled\u00ec 8 agosto 2012 1:31:03 GMT+10:00</span>\n"},
+            { "it", mediumStyle,   dt,     tzNone,     "7-ago-2012 17:31"},
+            { "it", mediumStyle,   dt,     tzGMT,      "7-ago-2012 15:31"},
+            { "it", mediumStyle,   dt,     tzCET,      "7-ago-2012 17:31"},
+            { "it", mediumStyle,   dt,     tzPDT,      "7-ago-2012 8:31"},
+            { "it", mediumStyle,   dt,     tzGMT10,    "8-ago-2012 1:31"},
+
+
+            { "en", longStyle,     dt,     tzNone,     "August 7, 2012 5:31:03 PM"},
+            { "en", longStyle,     dt,     tzGMT,      "August 7, 2012 3:31:03 PM"},
+            { "en", longStyle,     dt,     tzCET,      "August 7, 2012 5:31:03 PM"},
+            { "en", longStyle,     dt,     tzPDT,      "August 7, 2012 8:31:03 AM"},
+            { "en", longStyle,     dt,     tzGMT10,    "August 8, 2012 1:31:03 AM"},
+
+            { "it", longStyle,     dt,     tzNone,     "7 agosto 2012 17:31:03"},
+            { "it", longStyle,     dt,     tzGMT,      "7 agosto 2012 15:31:03"},
+            { "it", longStyle,     dt,     tzCET,      "7 agosto 2012 17:31:03"},
+            { "it", longStyle,     dt,     tzPDT,      "7 agosto 2012 8:31:03"},
+            { "it", longStyle,     dt,     tzGMT10,    "8 agosto 2012 1:31:03"},
+
+
+            { "en", fullStyle,     dt,     tzNone,     "Tuesday, August 7, 2012 5:31:03 PM CEST"},
+            { "en", fullStyle,     dt,     tzGMT,      "Tuesday, August 7, 2012 3:31:03 PM GMT"},
+            { "en", fullStyle,     dt,     tzCET,      "Tuesday, August 7, 2012 5:31:03 PM CEST"},
+            { "en", fullStyle,     dt,     tzPDT,      "Tuesday, August 7, 2012 8:31:03 AM PDT"},
+            { "en", fullStyle,     dt,     tzGMT10,    "Wednesday, August 8, 2012 1:31:03 AM GMT+10:00"},
+
+            { "it", fullStyle,     dt,     tzNone,     "marted\u00ec 7 agosto 2012 17:31:03 CEST"},
+            { "it", fullStyle,     dt,     tzGMT,      "marted\u00ec 7 agosto 2012 15:31:03 GMT"},
+            { "it", fullStyle,     dt,     tzCET,      "marted\u00ec 7 agosto 2012 17:31:03 CEST"},
+            { "it", fullStyle,     dt,     tzPDT,      "marted\u00ec 7 agosto 2012 8:31:03 PDT"},
+            { "it", fullStyle,     dt,     tzGMT10,    "mercoled\u00ec 8 agosto 2012 1:31:03 GMT+10:00"},
           };
       }
   }
